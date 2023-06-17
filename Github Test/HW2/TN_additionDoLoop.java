911955589404c38bcea48c0a51006896c6a767eb
Nguyentnt <134568442+Nguyentnt@users.noreply.github.com>
2023-06-16 21:54:01 -0400

    Uploading HW1 & HW2 to correct folder and file name
    Addition of Do Loop. While Loop, and For Loop. Renaming HW1 to correct naming convention.  I hope that I am uploading this to Github right with the push pull request :)

---
+++ Github Test/HW2/TN_additionDoLoop.java
@@ -0,0 +1,54 @@
+//Import Scanner util & Random uti
+import java.util.Scanner;
+import java.util.Random;
+
+public class TN_additionDoLoop{
+
+public static void main(String[] args){
+
+//Scanner object for input & Random numbers
+   Scanner input = new Scanner (System.in);
+   Random guess = new Random();
+   
+/*Declaration variables for needed data: two single digit numbers, anwser from student, correct
+anwser, count of questions, number of right & wrong, lastly string question*/
+   int answer;
+   int count= 0;
+   int right= 0;
+   int wrong= 0;
+   int i = 0;
+   
+//For do loop statement & declaring  variables for needed data: two single digit numbers,
+   
+   do {
+      int n1 = guess.nextInt(10);
+      int n2 = guess.nextInt(10);
+      int correctAnswer = n1 + n2;
+      String question = ("What is "+ n1 + "+" + n2 + "?");
+        
+   //Students input for math question
+      System.out.println(question);
+      answer = input.nextInt();
+      i++;
+      
+   
+      if(answer == correctAnswer){
+         System.out.println("true");
+         count++;
+         right++;
+      }
+      
+      else{
+         System.out.println("false");
+         count++;
+         wrong++;
+      }      
+      }
+      
+      while (i < 10);
+   //Tally up of number of questions counted, right & wrong 
+      System.out.println("Number of questions counted:  "+ count);
+      System.out.println("Number of questions wrong:  "+ wrong);
+      System.out.println("Number of questions right:  "+ right);   
+      }
+   }
---
+++ Github Test/HW2/TN_additionForLoop.java
@@ -0,0 +1,51 @@
+//Import Scanner util & Random uti
+import java.util.Scanner;
+import java.util.Random;
+
+public class TN_additionForLoop{
+
+public static void main(String[] args){
+
+//Scanner object for input & Random numbers
+   Scanner input = new Scanner (System.in);
+   Random guess = new Random();
+   
+/*Declaration variables for needed data: two single digit numbers, anwser from student, correct
+anwser, count of questions, number of right & wrong, lastly string question*/
+   int answer;
+   int count= 0;
+   int right= 0;
+   int wrong= 0;
+
+//For loop statement & declaring  variables for needed data: two single digit numbers,
+   for(int i=0; i<10; i++) {
+      int n1 = guess.nextInt(10);
+      int n2 = guess.nextInt(10);
+      int correctAnswer = n1 + n2;
+      String question = ("What is "+ n1 + "+" + n2 + "?");
+        
+   //Students input for math question
+      System.out.println(question);
+      answer = input.nextInt();
+      
+   
+      if(answer == correctAnswer){
+         System.out.println("true");
+         count++;
+         right++;
+      }
+      
+      else{
+         System.out.println("false");
+         count++;
+         wrong++;
+      }      
+      }
+      
+   //Tally up of number of questions counted, right & wrong 
+      System.out.println("Number of questions counted:  "+ count);
+      System.out.println("Number of questions wrong:  "+ wrong);
+      System.out.println("Number of questions right:  "+ right);   
+      }
+   }
+   
